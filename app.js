b6a1a953766bb91bc2b8c47d71f9f35c7468c37d
Nguyễn Trọng Đại <ngtrdai290197@gmail.com>
2018-10-01 16:42:49 +0700

    update

--- app.js
+++ app.js
@@ -2,27 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var database = require("./config/database");
-var cors = require("cors");
 var app = express();
-var server = require("http").Server(app);
-var socketio = require('socket.io')(server);
-var ioController = require('./controllers/socketio.controller')(socketio);
 var userRouter = require("./router/user.router");
-var uploader = require("./router/upload.router");
 var port = process.env.PORT || 3000;
 app.use(morgan("dev"));
-app.use(cors());
-app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 database.dbConnection();
 
 
-app.use("/api", uploader());
-
 app.use("/users", userRouter());
 
 
-server.listen(port, function () {
+app.listen(port, function () {
     console.log(`Nodejs listening at port: ${port}`);
 });
