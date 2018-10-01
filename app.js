99848ebac6884aa7051473e10eb32f5f45a82990
Nguyễn Trọng Đại <ngtrdai290197@gmail.com>
2018-10-01 16:49:40 +0700

    update

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var database = require("./config/database");
+var cors = require("cors");
+
 var app = express();
 var userRouter = require("./router/user.router");
 var port = process.env.PORT || 3000;
+app.use(cors());
+app.options('*', cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 database.dbConnection();
-
 
 app.use("/users", userRouter());
 
