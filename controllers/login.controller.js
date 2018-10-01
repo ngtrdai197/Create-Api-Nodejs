2ba3fc3a57c6dd710ce3aa2a8be34677cb753b7f
Nguyễn Trọng Đại <ngtrdai290197@gmail.com>
2018-10-01 16:46:06 +0700

    update router login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,30 +1,12 @@
 var User = require("../models/user.model");
-var bcrypt = require("bcrypt");
-var secure = require("../config/jwt");
-var jwt = require("jsonwebtoken");
+
 module.exports = {
     loginUser: function (req, res) {
         User.findOne({ 'username': req.body.username }).then(user => {
             if (user) {
-                var invalidPassword = bcrypt.compareSync(req.body.password, user.password);
-                if ((req.body.username == user.username) && invalidPassword) {
-                    var payload = {
-                        username: user.username,
-                        password: user.password,
-                        fullname: user.fullname,
-                        email: user.email,
-                        _id: user._id
-                    };
-                    var token = jwt.sign(payload, secure.secret, { expiresIn: 3600 });
-                    jwt.verify(token, secure.secret, function (err, decoded) {
-                        if (err) {
-                            return res.status(404).json({ status: false });
-                        } else {
-                            return res.status(200).json({ token: token, status: true, user: user.fullname });
-                        }
-                    });
+                if ((req.body.username == user.username) && (req.body.password == user.password)) {
+                    return res.status(200).json(user);
                 }
-                return res.end();
             }
             return res.status(404).send({
                 message: "User not found with id " + req.params.id
