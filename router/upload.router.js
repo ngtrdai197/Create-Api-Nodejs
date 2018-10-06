07500386660ee17589663cfbdd7fc525f166b232
Nguyen Trong Dai <n15dccn062@student.ptithcm.edu.vn>
2018-10-06 22:24:52 +0700

    Get file name uploaded

--- router/upload.router.js
+++ router/upload.router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var multer = require('multer');
 var router = express.Router();
+var fs = require('fs');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/uploads');
@@ -11,8 +12,17 @@ var storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage });
 module.exports = function () {
-    router.post("/upload", upload.any(), function (req, res) {
-        res.send(req.files);
+    router.post("/upload", upload.any(), function (req, res, err) {
+        res.status(200).send({message: 'Success'});
     });
+    router.get("/files", function (req, res) {
+        fs.readdir('./public/uploads', (err, files)=>{
+            res.send(files);
+        });
+    });
+    // router.get("/download:filename", function (req, res) {
+    //     var filename = req.params.filename;
+    //     res.download('./public/uploads' + filename);
+    // });
     return router;
 }
